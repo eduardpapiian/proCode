487bc1ad2a37373a64d8a796cf1a0b1b344484e3
eduardpapiian <mikroffon@gmail.com>
2019-08-28 10:01:00 +0300

    added routes

--- 1/server.js
+++ 1/server.js
@@ -1,27 +1,11 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
 const os = require('os');
+var app = require('./app');
 
 
 
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-    console.log(req.method);
-    console.log(req.headers);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(`
-    <!doctype>
-        <html>
-            <head>
-                <meta charset="utf-8">
-                <title>Main Page</title>
-            </head>
-            <body>
-                <a href="/about">about</a>
-            </body>
-            </html>
-    `);
-}).listen(3000, () => {
+const server = http.createServer(app.handleRequest).listen(3000, () => {
     console.log('START SERVER')
     var date = new Date();
     var timestamp = date.getTime();
