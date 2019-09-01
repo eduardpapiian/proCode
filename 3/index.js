dd7c43589668cf355822c640d7e9faab8c6b45ef
eduard <mikroffon@gmail.com>
2019-09-01 16:26:45 +0300

    hw third

--- 3/index.js
+++ 3/index.js
@@ -1,8 +1,10 @@
-const {readFile} = require('./utils/readFile')
+const {readFile, readAndWriteTo} = require('./utils/readFile')
 const path = require('path');
 
 readFile(path.join(__dirname, '/files/file1.txt')).then(text => console.log('FILE TEXT:', text))
     .catch(err => console.log('ERROR', err))
+
+readAndWriteTo(path.join(__dirname, '/files/file1.txt'), path.join(__dirname, '/files/file2.txt'))
 
 
 console.log('index started')
--- 3/utils/readFile.js
+++ 3/utils/readFile.js
@@ -1,21 +1,43 @@
 const fs = require('fs');
-
+var date = new Date();
+var timestamp = date.getTime();
 
 function readFile (url) {
     return new Promise((resolve, reject) => {
         fs.readFile(url, 'utf-8', (err, data ) =>{
             if(data){
-                console.log('DATA IN PROMISE', data)
+                // console.log('DATA IN PROMISE', data)
                 resolve(data)
             }else{
-                console.log('ERROR PROMISE', err)
+                // console.log('ERROR PROMISE', err)
                 reject(err)
             }
         })
     })
 }
 
+function readAndWriteTo (readUrl, writeUrl) {
+    return new Promise ((resolve, reject) => {
+        fs.readFile(readUrl, 'utf-8', (err, data ) =>{
+            if(data){
+                console.log('DATA IN PROMISE', data);
+                fs.appendFile(writeUrl, timestamp + data, (err) => {
+                    if (err) {
+                        reject({ status: 'Write error' })
+                    }else {
+                        resolve('File appended succesfuly');
+                    }
+                });
+            }else{
+                console.log('ERROR PROMISE', err);
+                reject({ status: 'Read error' })
+            }
+        })
+    }).then(data => console.log(data)).catch( err => console.log('ERROR', err))
+}
+
 
 module.exports = {
-    readFile: readFile
+    readFile: readFile,
+    readAndWriteTo: readAndWriteTo
 }
