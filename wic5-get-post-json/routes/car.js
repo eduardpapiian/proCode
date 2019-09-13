e3d3245fd807aab675cf881c91a55f55cdf2bb82
eduardpapiian <mikroffon@gmail.com>
2019-09-13 09:02:31 +0300

    fixed wic5

--- wic5-get-post-json/routes/car.js
+++ wic5-get-post-json/routes/car.js
@@ -4,11 +4,18 @@ const data = require('../storage/cars.json')
 
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
-    data.data.forEach(val =>{
-        if(val.id === Number(req.params.id)){
-            res.send(val)
-        }
-    })
+    let result = findCar(data.data, req.params.id)
+    result ? res.send(result) : res.status(404).send('Not found');
 });
+
+function findCar(arr, val){
+    console.log('arr', arr, 'val', val)
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].id === Number(val)) {
+            return arr[i]
+        }
+    }
+    return false
+}
 
 module.exports = router;
