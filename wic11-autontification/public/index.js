cbfe2497982106fd8661f4aaf11cbbec6ccc6aa9
eduardpapiian <eduardpapiian@gmail.com>
2019-10-08 20:41:52 +0300

    wic 11 done CRUD

--- wic11-autontification/public/index.js
+++ wic11-autontification/public/index.js
@@ -1,9 +1,13 @@
 
 const loginBtn = document.getElementById('login-btn');
 const registerBtn = document.getElementById('register-btn');
+const updateBtn = document.getElementById('update-btn');
+const deleteBtn = document.getElementById('delete-btn');
 
 loginBtn.addEventListener("click", login)
 registerBtn.addEventListener("click", register)
+updateBtn.addEventListener("click", update)
+deleteBtn.addEventListener("click", deleted)
 
 function login () {
   const data = {
@@ -26,6 +30,33 @@ function register () {
     password: document.querySelector("#password-reg").value
   };
   axios.post('/api/register', data).then(res => {
+    console.log(res);
+    if(res.status === 200){
+      console.log('res', res)
+    }
+  })
+    .catch(error => console.error('error', error));
+}
+
+function update () {
+  const data = {
+    login: document.querySelector("#login-upd").value,
+    email: document.querySelector("#email-upd").value,
+  };
+  axios.post('/api/update', data).then(res => {
+    console.log(res);
+    if(res.status === 200){
+      console.log('res', res)
+    }
+  })
+    .catch(error => console.error('error', error));
+}
+
+function deleted () {
+  const data = {
+    email: document.querySelector("#email-del").value,
+  };
+  axios.post('/api/delete', data).then(res => {
     console.log(res);
     if(res.status === 200){
       console.log('res', res)
--- wic11-autontification/routes/api.js
+++ wic11-autontification/routes/api.js
@@ -26,14 +26,46 @@ router.post('/register', function(req, res, next) {
   UserModel.findOne({email:email}).then(data => {
     if(data){
       res.render('respPage', {message: 'USER ALREADY EXIST'})
+    }else{
+      const user = UserModel(req.body)
+      user.save(function (err) {
+        if (err) console.log(err);
+        // saved!
+        res.render('respPage', {message: 'USER SAVED'})
+      })
     }
   })
-  const user = UserModel(req.body)
-  user.save(function (err) {
-    if (err) console.log(err);
-    // saved!
-    res.render('respPage', {message: 'USER SAVED'})
-  })
+});
+
+router.post('/update', function(req, res, next) {
+  const { email, login } = req.body
+  console.log('email',email)
+  console.log('login',login)
+  UserModel.findOneAndUpdate({email:email}, {login: login}).then(
+    data => {
+      console.log('data', data)
+      if(data !== null){
+        res.render('respPage', {message: 'USER Updated'})
+      }else{
+        res.render('respPage', {message: 'USER NOT FIND'})
+      }
+    }
+  ).catch(err => console.log(err))
+});
+
+router.post('/delete', function(req, res, next) {
+  const { email } = req.body
+  console.log('email',email)
+  UserModel.findOneAndDelete({email:email}).then(
+    data => {
+      console.log('data', data)
+      if(data !== null){
+        res.render('respPage', {message: 'USER DELETED'})
+      }else{
+        res.render('respPage', {message: 'USER NOT FIND'})
+      }
+    }
+  ).catch(err => res.sendStatus(404))
 });
 
 module.exports = router;
