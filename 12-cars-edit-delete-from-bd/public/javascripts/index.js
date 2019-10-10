6520fe980b01dfa53da8e16840acf23b022b06fa
eduardpapiian <mikroffon@gmail.com>
2019-10-10 16:10:42 +0300

    done hw 12

--- 12-cars-edit-delete-from-bd/public/javascripts/index.js
+++ 12-cars-edit-delete-from-bd/public/javascripts/index.js
@@ -1,33 +1,52 @@
 
-const addBtn = document.getElementById('add-btn');
+const addBtn = document.getElementById('add-car-btn');
 
 
 addBtn.addEventListener("click", add)
-
-function add () {
-  const data = {
-    email: document.querySelector("#email").value,
-    password: document.querySelector("#password").value
-  };
-  axios.post('/api/add', data).then(res => {
-    console.log(res);
-    if(res.status === 200){
-      console.log('res', res)
-    }
-  })
-    .catch(error => console.error('error', error));
-}
 
 function deleteCar(id){
   const data = {
     id: id
   }
   axios.post('/api/delete', data).then(res => {
-    console.log(res);
     if(res.status === 200){
-      console.log('res', res)
+      location.reload()
     }
   })
     .catch(error => console.error('error', error));
 }
+function changePrice(id){
+  var newPrice = prompt("Please enter your price", 99999);
+  if (newPrice != null || typeof newPrice === 'number') {
+    const data = {
+      id: id,
+      newPrice: newPrice
+    }
+    axios.post('/api/update', data).then(res => {
+      if(res.status === 200){
+        location.reload()
+      }
+    })
+      .catch(error => console.error('error', error));
+  }else{
+    alert('Enter YOR PRICE!')
+  }
+}
+
+function add () {
+  const data = {
+    manufacturer: document.querySelector("#manufacturer").value,
+    model: document.querySelector("#model").value,
+    price: document.querySelector("#price").value
+  };
+  axios.post('/api/create', data).then(res => {
+    console.log(res);
+    if(res.status === 200){
+      console.log('res', res)
+      location.reload()
+    }
+  })
+    .catch(error => console.error('error', error));
+}
+
 
--- 12-cars-edit-delete-from-bd/routes/api.js
+++ 12-cars-edit-delete-from-bd/routes/api.js
@@ -17,11 +17,40 @@ router.post('/delete', function(req, res, next) {
   ).catch(err => res.sendStatus(404))
 });
 
-router.get('/carList', function(req, res, next) {
+router.post('/update', function(req, res, next) {
+  const { id , newPrice } = req.body
+  CarModel.findOneAndUpdate({id:id}, {price: newPrice}).then(
+    data => {
+      console.log('data', data)
+      if(data !== null){
+        res.send('ok')
+      }else{
+        res.send('cant find car')
+      }
+    }
+  ).catch(err => res.sendStatus(404))
+});
+
+router.post('/create', function(req, res, next) {
   CarModel.find({}).then(data => {
     console.log('data', data)
-    res.send(data)
-  }).catch(err => console.log(err));
+    const arg = Number(data[data.length - 1].id) + 1;
+    console.log('arg', arg)
+    CarModel.findOne({id:arg}).then(data => {
+      if(data){
+        res.sendStatus(404)
+      }else{
+        req.body.id = arg
+        const car = CarModel(req.body)
+        car.save(function (err) {
+          if (err) console.log(err);
+          // saved!
+          res.send('ok')
+        })
+      }
+    })
+  }).catch(err => console.log(err))
 });
+
 
 module.exports = router;
