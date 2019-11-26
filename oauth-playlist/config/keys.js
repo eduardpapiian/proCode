176762b013d3c333cac309462caa824783cfc230
eduardpapiian <mikroffon@gmail.com>
2019-11-26 12:18:00 +0200

    remove api keys

--- oauth-playlist/config/keys.js
+++
@@ -1,8 +0,0 @@
-// add this file to .gitignore
-
-module.exports = {
-  google: {
-    clientID: '148526783851-if8uosr4kmegjfgd0dftl2j6p1b7ctin.apps.googleusercontent.com',
-    clientSecret: 'y3kw60Agl5ZKEipTK3Z7FP0T'
-  }
-};
