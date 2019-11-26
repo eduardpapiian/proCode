cd634591c4b1510ce48853dd9e72ba7c28aa4233
eduardpapiian <mikroffon@gmail.com>
2019-11-26 12:12:17 +0200

    added express-generator

---
+++ oauth-playlist/config/keys.js
@@ -0,0 +1,8 @@
+// add this file to .gitignore
+
+module.exports = {
+  google: {
+    clientID: '148526783851-if8uosr4kmegjfgd0dftl2j6p1b7ctin.apps.googleusercontent.com',
+    clientSecret: 'y3kw60Agl5ZKEipTK3Z7FP0T'
+  }
+};
