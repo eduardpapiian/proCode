1b3b1232842d9ffb65fec40c5eecb34e0701ad62
eduardpapiian <mikroffon@gmail.com>
2019-11-28 13:33:30 +0200

    done google auth authentication

--- oauth-playlist/app.js
+++ oauth-playlist/app.js
@@ -6,17 +6,32 @@ var lessMiddleware = require('less-middleware');
 var logger = require('morgan');
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
+const passportSetup = require('./config/passport-setup');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
 
 //connect to mongodb
 mongoose.connect(keys.mongodb.dbURI, ()=> {
   console.log('connected to mongo db')
 })
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const authRoutes = require('./routes/auth-routes');
-const passportSetup = require('./config/passport-setup');
+const profileRoutes = require('./routes/profile-routes');
 
 var app = express();
+
+// set up session cookies
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [keys.session.cookieKey]
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -34,6 +49,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/auth', authRoutes);
+
+app.use('/profile', profileRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- oauth-playlist/routes/auth-routes.js
+++ oauth-playlist/routes/auth-routes.js
@@ -3,14 +3,16 @@ const passport = require('passport')
 
 //auth login
 router.get('/login', (req, res) => {
-  res.render('login')
+  res.render('login', { user: req.user })
 });
 
 
 // auth logout
 router.get('/logout', (req, res) => {
   //handle with passport
-  res.send('logging out')
+  // res.send('logging out')
+  req.logout();
+  res.redirect('/')
 });
 
 //auth with google
@@ -20,7 +22,8 @@ router.get('/google', passport.authenticate('google', {
 
 //callback route for google to redirect to
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-  res.send('you reached the callback URI');
+  // res.send(req.user);
+  res.redirect('/profile')
 });
 
 module.exports = router
