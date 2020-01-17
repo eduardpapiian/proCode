b2352a6f102b68d7d1c7ee20accd5a4d1eb6fe49
eduardpapiian <mikroffon@gmail.com>
2020-01-17 13:52:36 +0200

    done dz 14.01 korpachov

--- 14.01-karpachov/src/components/TaskModule/index.jsx
+++ 14.01-karpachov/src/components/TaskModule/index.jsx
@@ -1,5 +1,11 @@
 import React, {Component} from 'react';
 import rectangle from './img/Polygon.svg';
+import checkItem from './img/checkItem.svg';
+import rightIcon1 from './img/Vector.svg';
+import rightIcon2 from './img/Vector2.svg';
+import rightIcon3 from './img/Vector3.svg';
+import rightIcon4 from './img/Vector4.svg';
+import rightIcon5 from './img/Vector5.svg';
 import './index.scss';
 
 class TaskModule extends Component {
@@ -29,6 +35,13 @@ class TaskModule extends Component {
         }
       ]
     }
+  }
+  test(checked, index){
+    const list = this.state.taskList
+    list[index].selected = !checked
+    this.setState({
+      taskList: list
+    })
   }
   componentWillMount(){
     console.log('will mount!!')
@@ -60,9 +73,11 @@ class TaskModule extends Component {
                     {item.text}
                   </div>
                 </div>
-                <div className="checkbox-block">
 
+                <div className={'checkbox-block ' + (item.selected ? 'checked' : '')} onClick={() => this.test(item.selected, index)}>
+                  {item.selected ? (<img src={checkItem} alt=""/>) : ('')}
                 </div>
+
               </div>
             ))}
 
@@ -70,16 +85,19 @@ class TaskModule extends Component {
 
           <div className="right-side-block">
             <div className="item">
-              <img src="" alt=""/>
+              <img src={rightIcon1} alt=""/>
             </div>
             <div className="item">
-              <img src="" alt=""/>
+              <img src={rightIcon2} alt=""/>
             </div>
             <div className="item">
-              <img src="" alt=""/>
+              <img src={rightIcon3} alt=""/>
             </div>
             <div className="item">
-              <img src="" alt=""/>
+              <img src={rightIcon4} alt=""/>
+            </div>
+            <div className="item">
+              <img src={rightIcon5} alt=""/>
             </div>
           </div>
 
