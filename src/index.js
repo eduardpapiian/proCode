e3660b45ea308cfa07aa0ba5a8464f7fc48c23d9
eduardpapiian <eduardpapiian@gmail.com>
2020-01-28 22:02:46 +0200

    wic 11 done CRUD

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+import './index.css';
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import listApp from './reducers'
+import App from './App';
+const store = createStore(listApp)
+render(<App store={store} />, document.getElementById('root'))
 serviceWorker.unregister();
